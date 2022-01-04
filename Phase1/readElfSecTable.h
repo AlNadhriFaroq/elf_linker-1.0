daa0754af532021a7b22107c11f8da5f59f4083b
Dorian Casagrande <95753037+DorianCasa@users.noreply.github.com>
2022-01-04 17:03:27 +0100

    Update readElfSecTable.h
    Ajout de commentaires et spécifications

--- Phase1/readElfSecTable.h
+++ Phase1/readElfSecTable.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include "readElfHeader.h"
 
+// Documentation chapitre 1-10 Figure 1-8
 typedef struct {
   uint32_t sh_name;
   uint32_t sh_type;
@@ -17,6 +18,44 @@ typedef struct {
   uint64_t sh_entsize;
 } Elf64_Shdr;
 
-void affiche_section_table(FILE *elfFile,Elf64_Ehdr header);
+/***************************************
+ * find_type                      
+ * parametres : long num, char *sh_type 
+ * resultat : aucun                        
+ * description : associe à sh_type la valeur textuelle correspondant a la valeur de num,
+ *               num etant le type d'une section 
+ * effet de bord : remplissage de la valeur de sh_type               
+ ***************************************/
+void find_type(long num, char *sh_type);
+
+/***************************************
+ * revstr                      
+ * parametres : char *str 
+ * resultat : aucun                        
+ * description : Transforme une chaine de caracteres donnee en parametre en sa valeur inverse/mirroir
+ *               (Exemple : Noel -> leoN)
+ * effet de bord : modification de str              
+ ***************************************/
+void revstr(char *str);
+
+/***************************************
+ * find_flags                      
+ * parametres : char *tab, int n
+ * resultat : aucun                        
+ * description : Dans une section, des instructions sont lues et des actions sont effectuees 
+ *               Des fanions sont associees a ces actions. (Exemple des fanions : ZNCV)
+ *               Cette fonction remplit le tableau tab avec la valeur des fanions
+ * effet de bord : remplissage de tab          
+ ***************************************/
+void find_flags(char *tab, int n);
+
+/***************************************
+ * affiche_section_table                      
+ * parametres : FILE *elfFile, Elf64_Ehdr header
+ * resultat : aucun                        
+ * description : Affichage de la table des sections et des informations sur chacune d'entre elles
+ * effet de bord : aucun         
+ ***************************************/
+void affiche_section_table(FILE *elfFile, Elf64_Ehdr header);
 
 #endif
