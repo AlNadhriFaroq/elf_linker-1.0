a2291e3420043f491179cf860a462e3616d1d98f
landryuga <95070795+landryuga@users.noreply.github.com>
2022-01-06 21:07:45 +0100

    Update readElfSym.c
    avancee etape, recuperation du type par st_name non par st_info

--- Phase1/readElfSym.c
+++ Phase1/readElfSym.c
@@ -5,26 +5,69 @@
 #include <stdlib.h>
 #include <string.h>
 
-void affichage_dynsym(FILE *elfFile, uint64_t offset, uint64_t entrees){
+void find_type_sym(uint32_t value, char *type){
+    switch (value){
+        case 0:
+            strcpy(type, "NOTYPE");
+            break;
+        case 1:
+            strcpy(type, "OBJECT");
+            break;
+        case 2:
+            strcpy(type, "FUNC");
+            break;
+        case 3:
+            strcpy(type, "SECTION");
+            break;
+        case 4:
+            strcpy(type, "FILE");
+            break;
+        case 5:
+            strcpy(type, "COMMON");
+            break;
+        case 6:
+            strcpy(type, "TLS");
+            break;
+        case 10:
+            strcpy(type, "LOOS");
+            break;   
+        case 12:
+            strcpy(type, "HIOS");
+            break;
+        case 13:
+            strcpy(type, "LOPROC");
+            break;
+        case 15:
+            strcpy(type, "HIPROC");
+            break;
+        default:
+            break;
+        }
+}
+
+void affichage_dynsym(FILE *elfFile, uint64_t offset, uint64_t entrees, uint64_t taille_entree){
     fseek(elfFile, offset, SEEK_SET);
     Elf64_Sym sym;
     printf("   Num:    Value          Size Type    Bind   Vis      Ndx Name\n");
     for (int i = 0; i < entrees; i++){
         fread(&sym, 1, sizeof(sym), elfFile);
-        printf("    %d:   %016lx     %lu       %d       %d        %lu \n", i, sym.st_value, sym.st_size, ELF64_ST_TYPE(sym.st_info), ELF64_ST_BIND(sym.st_info), sym.st_name);
+        printf("    %d:   %016lx     %lu       %d       %d        %lu \n", i, sym.st_value, sym.st_size, ELF32_ST_TYPE(sym.st_info), ELF32_ST_BIND(sym.st_info), sym.st_name);
     }
 }
 
-void affichage_symtab(FILE *elfFile, uint64_t offset, uint64_t entrees){
+
+void affichage_symtab(FILE *elfFile, uint64_t offset, uint64_t entrees, uint64_t taille_entree){
     assert(fseek(elfFile, offset, SEEK_SET) == 0);
     printf("l'adresse de la table des symboles est %lx\n", offset);
-    
-    Elf64_Sym sym;
-    printf("   Num:    Value          Size Type    Bind   Vis      Ndx Name\n");
+    Elf32_Sym sym;
+    int tmp;
+    printf("   Num:    Value               Size     Type    Bind       Ndx Name\n");
     for (int i = 0; i < entrees; i++){
+        char type[10];
+        assert(fread(&tmp, 1, taille_entree - sizeof(sym), elfFile) != 0);
         assert(fread(&sym, 1, sizeof(sym), elfFile) != 0);
-        //printf("%d\n", ELF64_ST_TYPE(sym.st_info));
-        printf("    %2d:   %016lx     %lu       %d       %d        %lu \n", i, sym.st_value, sym.st_size, ELF64_ST_TYPE(sym.st_info), ELF64_ST_BIND(sym.st_info), sym.st_name);
+        find_type_sym(ELF32_ST_TYPE(sym.st_name), type);
+        printf("    %2d:   %016x     %u       %s       %x        %x \n", i, sym.st_value, sym.st_size, type, ELF32_ST_BIND(sym.st_name), sym.st_name);
     }
 }
 
@@ -68,13 +111,13 @@ void affiche_symboles(FILE *elfFile, Elf64_Ehdr header)
         if (find_type_symbole(sectHdr.sh_type) == DYNSYM){
             uint64_t entrees =  (sectHdr.sh_size / sectHdr.sh_entsize);
             printf("Symbol table '%s' contains %lu entries:\n", sh_name, entrees);
-            affichage_dynsym(elfFile, sectHdr.sh_offset, entrees);
+            affichage_dynsym(elfFile, sectHdr.sh_offset, entrees, sectHdr.sh_entsize);
             printf("\n");
         }
         else if (find_type_symbole(sectHdr.sh_type) == SYMTAB){
             uint64_t entrees =  (sectHdr.sh_size / sectHdr.sh_entsize);
             printf("Symbol table '%s' contains %lu entries:\n", sh_name, entrees);
-            affichage_symtab(elfFile, sectHdr.sh_offset, entrees);
+            affichage_symtab(elfFile, sectHdr.sh_offset, entrees, sectHdr.sh_entsize);
             printf("\n");
         }
         else{
