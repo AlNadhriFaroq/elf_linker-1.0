93b5605543c63805949eab7ebc095f7fd076a06e
Sakraft <95034115+Sakraft@users.noreply.github.com>
2022-01-07 13:19:44 +0100

    MAJ des lectures d'options
    Correction et ajout de prise en compte de cas spéciaux dans la lecture d'option.
    Ajout des commentaires et spécifications des modules d'options.

---
+++ readelf.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "readElfHeader.h"
+#include "readElfSecTable.h"
+#include "readElfLecSect.h"
+#include "readElfSym.h"
+#include "readElfRel.h"
+#include "options.h"
+
+int main(int argc, char *argv[])
+{
+	// Lecture et interpretation des options entrees
+	options Opt = read_options(argc, argv);
+
+	// Parcours des fichiers a afficher
+	for (int i = 0 ; i < Opt.nb_file ; i++)
+	{
+		// Ouverture du fichier courant
+		FILE *elfFile;
+		elfFile = fopen(Opt.fileList[i], "rb");
+		
+		// Cas ou le fichier presente une erreur
+		if (elfFile == NULL)
+		{
+			printf("readelf: ERROR: « %s »: File open error!\n", Opt.fileList[i]);
+		}
+		
+		// Cas ou le fichier s'est ouvert normalement
+		else
+		{
+			// Lecture de l'entete du fichier
+			Elf64_Ehdr header;
+			fread(&header, 1, sizeof(header), elfFile); 
+
+			// Verification que le fichier lu est bien un fichier ELF
+			if (!(header.e_ident[0] == 0x7f && header.e_ident[1] == 'E' &&
+				header.e_ident[2] == 'L' && header.e_ident[3] == 'F'))
+			{
+				printf("readelf: Error: Not an ELF file - it has the wrong magic bytes at the start\n");
+				fclose(elfFile);
+				exit(1);
+			}
+
+			// Execution des fonctions correspondants aux options entrees en parametre
+			if (Opt.h)
+			{
+				affiche_header(header);
+			}
+			if (Opt.S)
+			{
+				affiche_section_table(elfFile, header);
+			}
+			if (Opt.s)
+			{
+				affiche_symboles(elfFile, header);
+			}
+			if (Opt.r)
+			{
+				affiche_reimplantation_table(elfFile, header);
+			}
+			if (Opt.nb_sec > 0)
+			{
+				// Parcours des sections a afficher
+				for (int s = 0 ; s < Opt.nb_sec ; s++)
+				{
+					affiche_section(elfFile, header, Opt.secList[s]);
+				}
+			}
+
+			fclose(elfFile);
+		}
+	}
+	
+	return 0;
+}
