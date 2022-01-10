0ae262529c273c12f2ab1b3c332cf6c1ee89e757
Faroq Al-Nadhari <farouq71548@gmail.com>
2022-01-10 17:30:09 +0100

    modification etape 6 qui marche pas

--- Phase2/renumSect.c
+++ Phase2/renumSect.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include "../Phase1/readelfOptions.h"
 #include "../Phase1/readelfHeader.h"
 #include "../Phase1/readelfSectTable.h"
+#include "../Phase1/readelfSectLect.h"
 
 #define SH_RELA 4
 #define SH_REL 9
@@ -47,43 +49,59 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	int nbSecSupp = 0;
-	long idx = 0x0;
+	//int nbSecSupp = 0;
+	long idx = 0;
 	Elf32_Ehdr hdr;
 	Elf32_Phdr progHdr;
 	Elf32_Shdr sectHdr;
 
 	fread(&hdr, 1, sizeof(hdr), elfFile);
+	fwrite(&hdr, 1, sizeof(hdr), outFile);
 
 	//lecture et ecriture de program header if exists
-	fread(&progHdr, 1, sizeof(progHdr), elfFile);
-	fwrite(&progHdr, 1, sizeof(progHdr), outFile);
+	//fread(&progHdr, 1, sizeof(progHdr), elfFile);
+	//fwrite(&progHdr, 1, sizeof(progHdr), outFile);
 
 	fseek(elfFile, sizeof(hdr) + sizeof(progHdr), SEEK_SET);
 
-	uint8_t *sec = NULL;
 	fseek(elfFile, hdr.e_shoff, SEEK_SET);
 	fread(&sectHdr, 1, sizeof(sectHdr), elfFile);
 
-	int err=0;
-	while (idx != hdr.e_shnum)
+	char c;
+	int jdx = sizeof(hdr);
+	while (jdx < sectHdr.sh_offset+0x154){
+		fseek(elfFile,jdx, SEEK_SET);
+		fread(&c, 1, sizeof(c), elfFile);
+		fwrite(&c,1,sizeof(c),outFile);
+		jdx++;
+	}
+
+	SectionsList sList = lire_sections_table(elfFile, hdr);
+	lire_sections(elfFile, sList);
+	char *list[] = {"", "-S", "-x", "2", "renumSect"};
+	Options op = lire_options(5, list);
+	afficher_sections_table(sList, hdr.e_shoff);
+	afficher_sections(sList, op) ;
+
+	while (idx < sList.nb_sect)
 	{
-		idx++;
-		err = fseek(elfFile, sectHdr.sh_offset, SEEK_SET); // seg fault in fseek
-		if (err != 0)
+		// ecriture section content
+		Section sect = sList.sectTab[idx];
+		int diff = sList.sectTab[idx + 1].header.sh_offset - (sect.header.sh_offset + sect.header.sh_size);
+
+		fwrite(sect.dataTab, 1, sect.header.sh_size, outFile);
+		printf("%p\n",&(sect.dataTab));
+		
+		if ((idx+1 <= sList.nb_sect) && (diff != 0))
 		{
-			printf("error\n");
+			c=0x0;
+			fwrite(&c, 1, sizeof(c), outFile);
 		}
 
-		// Lecture et ecriture section content
-		fread(sec, 1, sectHdr.sh_size, elfFile);
-		fwrite(sec, 1, sectHdr.sh_offset, outFile);
-
-		//Lecture section header
-		fseek(elfFile, hdr.e_shoff + idx * sizeof(sectHdr), SEEK_SET);
-		fread(&sectHdr, 1, sizeof(sectHdr), elfFile);
+		idx++;
 	}
-	for (int i = 0; i < hdr.e_shnum; i++)
+	// Lecture et ecriture section header
+	for (int i = 0; i < hdr.e_shnum ; i++)
 	{
 		fseek(elfFile, hdr.e_shoff + i * sizeof(sectHdr), SEEK_SET);
 		fread(&sectHdr, 1, sizeof(sectHdr), elfFile);
@@ -97,9 +115,10 @@ int main(int argc, char *argv[])
 		}*/
 	}
 
-	hdr.e_shnum -= nbSecSupp;
-	fseek(outFile, 0, SEEK_SET);
-	fwrite(&hdr, 1, sizeof(hdr), outFile);
+	supprimer_sections_table(sList);
+	//hdr.e_shnum -= nbSecSupp;
+	//fseek(outFile, 0, SEEK_SET);
+	//fwrite(&hdr, 1, sizeof(hdr), outFile);
 	fclose(elfFile);
 	fclose(outFile);
 	return 0;
