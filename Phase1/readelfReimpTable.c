a20ced72f4b2e74e3628c090996ae9b47a406c14
Sakraft <95034115+Sakraft@users.noreply.github.com>
2022-01-12 15:56:49 +0100

    MAJ étape 5
    Mise à jour des modules de l'étape 5 : correction sur les fonctions de lecture et d'affichage.
    Ajout des fonctions d'écriture pour l'étape 5.
    Ajout des commentaires et spécifications pour l'étape 5.

--- Phase1/readelfReimpTable.c
+++ Phase1/readelfReimpTable.c
@@ -72,93 +72,133 @@ static void lire_type(int num, char *type)
 }
 
 
-Rela lire_rela(Section sect, int i)
+/*	lire_rela_liste(Section sect, SectionsList liste_sect, SymbolesList liste_symb)
+		Lit une liste de reimplantation de type rela
+*/
+RelocList lire_rela_liste(Section sect, SectionsList liste_sect, SymbolesList liste_symb)
 {
-	Rela reimp = {"", {0, 0, 0}};
-	
-	// Lecture de reimp.rela dans la section sect entree
-	reimp.rela.r_offset = *((uint32_t *) &(sect.dataTab[i*12  ]));
-	reimp.rela.r_info   = *((uint32_t *) &(sect.dataTab[i*12+4])); 
-	reimp.rela.r_addend = *((int32_t  *) &(sect.dataTab[i*12+8])); 
-	
-	// Traduction de l'info en texte lisible
-	lire_type(ELF32_R_TYPE(reimp.rela.r_info), reimp.type);
-	
-	// Gestion de la negation de r_addend
-	if (reimp.rela.r_addend & ((uint32_t)1 << 31))
-	{
-		reimp.rela.r_addend = - reimp.rela.r_addend;
-	}
-		
-	return reimp;
-}
-
-
-Rel lire_rel(Section sect, int i)
-{
-	Rel reimp = {"", {0, 0}};
-	
-	// Lecture de reimp.rela dans la section sect entree
-	reimp.rel.r_offset = *((uint32_t *) &(sect.dataTab[i*8  ]));
-	reimp.rel.r_info   = *((uint32_t *) &(sect.dataTab[i*8+4])); 
-	
-	// Traduction de l'info en texte lisible
-	lire_type(ELF32_R_TYPE(reimp.rel.r_info), reimp.type);
-	
-	return reimp;
-}
-
-
-RelocList lire_rela_liste(Section sect)
-{
+	// Calcul du nombre de reimplentation
 	int nb_reloc = sect.header.sh_size / sect.header.sh_entsize;
+	
+	// Creation et initialisation d'une liste
 	RelocList liste_reimp = {"", sect.header.sh_offset, nb_reloc, 1, NULL, NULL};
 	strcpy(liste_reimp.name, sect.name);
 	liste_reimp.relaTab = malloc(sizeof(Rela) * nb_reloc);
 	
+	// Parcours des reimplantations
 	for (int i = 0; i < nb_reloc; i++)
 	{
-		liste_reimp.relaTab[i] = lire_rela(sect, i);
+		// Creation d'une reimplantation
+		Rela reimp = {0, "", "", {0, 0, 0}};
+	
+		// Lecture de reimp.rela dans la section sect entree
+		reimp.rela.r_offset = *((uint32_t *) &(sect.dataTab[i*12  ]));
+		reimp.rela.r_info   = *((uint32_t *) &(sect.dataTab[i*12+4])); 
+		reimp.rela.r_addend = *((int32_t  *) &(sect.dataTab[i*12+8])); 
+		
+		// Symbole associe a la reimplantation
+		Symbole symbole = liste_symb.symbTab[ELF32_R_SYM(reimp.rela.r_info)];
+	
+
+		// Traduction des informations precedentes en textes lisibles
+		reimp.value = symbole.symb.st_value,
+		lire_type(ELF32_R_TYPE(reimp.rela.r_info), reimp.type);
+		strcpy(reimp.name, symbole.name);
+		if (strcmp(reimp.name, "") == 0)
+		{
+			strcpy(reimp.name, liste_sect.sectTab[symbole.symb.st_shndx].name);
+		}
+
+		// Gestion de la negation de r_addend
+		if (reimp.rela.r_addend & ((uint32_t)1 << 31))
+		{
+			reimp.rela.r_addend = - reimp.rela.r_addend;
+		}
+		
+		// Ajout de la reimplantation courante dans la liste
+		liste_reimp.relaTab[i] = reimp;
 	}
 	
 	return liste_reimp;
 }
 
 
-RelocList lire_rel_liste(Section sect)
+/*	lire_rel_liste(Section sect, SectionsList liste_sect, SymbolesList liste_symb)
+		Lit une liste de reimplantation de type rel
+*/
+RelocList lire_rel_liste(Section sect, SectionsList liste_sect, SymbolesList liste_symb)
 {
+	// Calcul du nombre de reimplantation
 	int nb_reloc = sect.header.sh_size / sect.header.sh_entsize;
+	
+	// Creation et initialisation d'une liste
 	RelocList liste_reimp = {"", sect.header.sh_offset, nb_reloc, 0, NULL, NULL};
 	strcpy(liste_reimp.name, sect.name);
 	liste_reimp.relTab = malloc(sizeof(Rel) * nb_reloc);
-
+	
+	// Parcours des reimplantations
 	for (int i = 0; i < nb_reloc; i++)
 	{
-		liste_reimp.relTab[i] = lire_rel(sect, i);
+		// Creation d'une reimplantation
+		Rel reimp = {0, "", "", {0, 0}};
+	
+		// Lecture de reimp.rela dans la section sect entree
+		reimp.rel.r_offset = *((uint32_t *) &(sect.dataTab[i*8  ]));
+		reimp.rel.r_info   = *((uint32_t *) &(sect.dataTab[i*8+4])); 
+	
+		// Symbole associe a la reimplantation
+		Symbole symbole = liste_symb.symbTab[ELF32_R_SYM(reimp.rel.r_info)];
+	
+		// Traduction des informations precedentes en textes lisibles
+		reimp.value = symbole.symb.st_value,
+		lire_type(ELF32_R_TYPE(reimp.rel.r_info), reimp.type);
+		strcpy(reimp.name, symbole.name);
+		if (strcmp(reimp.name, "") == 0)
+		{
+			strcpy(reimp.name, liste_sect.sectTab[symbole.symb.st_shndx].name);
+		}
+		
+		// Ajout de la reimplantation courante dans la liste
+		liste_reimp.relTab[i] = reimp;
 	}
 	
 	return liste_reimp;
 }
 
 
-RelocTable lire_reimp_table(SectionsList liste_sect)
+/*	lire_reimp_table(SectionsList liste_sect, SymbolesList liste_symb)
+		Lit une table de reimplantation
+*/
+RelocTable lire_reimp_table(SectionsList liste_sect, SymbolesList liste_symb)
 {
 	RelocTable table_reimp = {0, NULL};
 	table_reimp.tab = malloc(sizeof(RelocList) * 0);
-
+	
+	// Parcours des sections
 	for (int i = 0; i < liste_sect.nb_sect; i++)
 	{
+		// Section courante
 		Section sect = liste_sect.sectTab[i];
+		
+		// Cas ou la section courante est du type SHT_RELA
 		if (sect.header.sh_type == 4)
 		{
-			RelocList liste_reimp = lire_rela_liste(sect);
+			// Lecture de la liste de reimplantation de la section courante
+			RelocList liste_reimp = lire_rela_liste(sect, liste_sect, liste_symb);
+			
+			// Reallocation et ajout de cette liste dans la table
 			table_reimp.nb_list++;
 			table_reimp.tab = realloc(table_reimp.tab, sizeof(RelocList) * table_reimp.nb_list);
 			table_reimp.tab[table_reimp.nb_list - 1] = liste_reimp;
 		}
+		
+		// Cas ou la section courante est du type SHT_REL
 		else if (sect.header.sh_type == 9)
 		{
-			RelocList liste_reimp = lire_rel_liste(sect);
+			// Lecture de la liste de reimplantation de la section courante
+			RelocList liste_reimp = lire_rel_liste(sect, liste_sect, liste_symb);
+			
+			// Reallocation et ajout de cette liste dans la table
 			table_reimp.nb_list++;
 			table_reimp.tab = realloc(table_reimp.tab, sizeof(RelocList) * table_reimp.nb_list);
 			table_reimp.tab[table_reimp.nb_list - 1] = liste_reimp;
@@ -169,13 +209,18 @@ RelocTable lire_reimp_table(SectionsList liste_sect)
 }
 
 
+/*	afficher_reimp_table(RelocTable table_reimp)
+		Affiche les informations de la table de reimplantation donnee
+*/
 void afficher_reimp_table(RelocTable table_reimp)
 {
 	// Parcours des liste de reimplantation
 	for (int i = 0; i < table_reimp.nb_list; i++)
 	{
+		// Liste courante
 		RelocList liste_reimp = table_reimp.tab[i];
 		
+		// Affichage du titre
 		if (liste_reimp.nb_reloc == 1)
 		{
 			printf("\nRelocation section '%s' at offset 0x%x contains 1 entry:\n",
@@ -190,20 +235,24 @@ void afficher_reimp_table(RelocTable table_reimp)
 		// Cas d'une liste de reimplantation de type rela
 		if (liste_reimp.type)
 		{
-			printf("Offset             Info           Type           Sym. index          + Addend \n");
+			printf(" Offset          Info           Type           Sym. Value    Sym. Name + Addend\n");
 			
 			// Parcours des reimplantations
 			for (int j = 0; j < liste_reimp.nb_reloc; j++)
 			{
+				// Reimplantation courante
 				Rela reimp = liste_reimp.relaTab[j];
 				
+				// Cas d'un r_addend negatif
 				if (reimp.rela.r_addend < 0)
 				{
 					printf("%06x    %06x      %-15s  %08x    - %x\n",
 						   reimp.rela.r_offset, reimp.rela.r_info, reimp.type,
-						   ELF32_R_SYM(reimp.rela.r_info), reimp.rela.r_addend);
+						   ELF32_R_SYM(reimp.rela.r_info), abs(reimp.rela.r_addend));
 				}
-				else{
+				// Cas d'un r_addend positif
+				else
+				{
 					printf("%06x    %06x      %-15s  %08x     + %x\n",
 						   reimp.rela.r_offset, reimp.rela.r_info, reimp.type,
 						   ELF32_R_SYM(reimp.rela.r_info), reimp.rela.r_addend);
@@ -219,28 +268,91 @@ void afficher_reimp_table(RelocTable table_reimp)
 			// Parcours des reimplantations
 			for (int j = 0; j < liste_reimp.nb_reloc; j++)
 			{
+				// Reimplantation courante
 				Rel reimp = liste_reimp.relTab[j];
+				
+				// Decoupage des caracteres finaux du nom s'il y en a trop
+				char name[22] = "";
+				strncpy(name, reimp.name, 22);
+				
+				// Affichage des informations de la reimplantation courante
 				printf("%08x  %08x %-17s %08x   %s\n",
-					   reimp.rel.r_offset, reimp.rel.r_info, reimp.type,
-					   ELF32_R_SYM(reimp.rel.r_info), "");
+					   reimp.rel.r_offset, reimp.rel.r_info, reimp.type, reimp.value, name);
 			}
 		}
 	}
 }
 
 
-// void ecrire_reimp_table(SectionsList liste_sect, RelocTable table_reimp)
+/*	ecrire_reimp_table(SectionsList liste_sect, RelocTable table_reimp)
+		Ecrit dans les sections dediees le contenu de la table de reimplantation donnee
+*/
+void ecrire_reimp_table(SectionsList liste_sect, RelocTable table_reimp)
+{
+	// Parcours des listes de reimplantations 
+	for (int i = 0; i < table_reimp.nb_list; i++)
+	{
+		// Liste courante
+		RelocList liste_reimp = table_reimp.tab[i];
+		
+		int j = 0;
+		// Parcours des sections
+		while (strcmp(liste_reimp.name, liste_sect.sectTab[j].name) != 0)
+		{
+			j++;
+		}
+		
+		// Section dans laquelle la liste de reimplantation doit etre ecrite
+		Section sect = liste_sect.sectTab[j];
+		
+		// Cas d'un liste de type rela
+		if (liste_reimp.type)
+		{
+			// Parcours des reimplantations
+			for (int j = 0; j < liste_reimp.nb_reloc; j++)
+			{
+				// Reimplantation courante
+				Rela reimp = liste_reimp.relaTab[j];
+				
+				// Ecriture de la reimplantation dans la section
+				*((uint32_t *) &(sect.dataTab[j*12  ])) = reimp.rela.r_offset;
+				*((uint32_t *) &(sect.dataTab[j*12+4])) = reimp.rela.r_info;
+				*((int32_t  *) &(sect.dataTab[j*12+8])) = reimp.rela.r_addend;
+			}
+		}
+		
+		// Cas d'une liste de type rel
+		else
+		{
+			// Parcours des reimplantations
+			for (int j = 0; j < liste_reimp.nb_reloc; j++)
+			{
+				// Reimplantation courante
+				Rel reimp = liste_reimp.relTab[j];
+				
+				// Ecriture de la reimplantation dans la saction
+				*((uint32_t *) &(sect.dataTab[j*8  ])) = reimp.rel.r_offset;
+				*((uint32_t *) &(sect.dataTab[j*8+4])) = reimp.rel.r_info;
+			}
+		}
+	}
+}
 
 
+/*	supprimer_reimp_table(RelocTable table_reimp)
+		Libere l'espace memoire de la table de reimplantation donnee
+*/
 void supprimer_reimp_table(RelocTable table_reimp)
 {
 	// Parcours des listes de reimplantations
 	for (int i = 0; i < table_reimp.nb_list; i++)
 	{
+		// Cas des listes de type rela 
 		if (table_reimp.tab[i].type)
 		{
 			free(table_reimp.tab[i].relaTab);
 		}
+		// Cas des listes de type rel
 		else
 		{
 			free(table_reimp.tab[i].relTab);
--- Phase1/readelfReimpTable.h
+++ Phase1/readelfReimpTable.h
@@ -6,11 +6,12 @@
 #include "readelfSectTable.h"
 #include "readelfSymbTable.h"
 
-#define ELF32_R_SYM(i) ((i) >> 16)
-#define ELF32_R_TYPE(i) ((unsigned char)(i))
+#define ELF32_R_SYM(i)     ((i) >> 8)
+#define ELF32_R_TYPE(i)    ((unsigned char)(i))
 #define ELF32_R_INFO(s, t) (((s) << 8) + (unsigned char)(t))
 
 
+// Structures pour stocker une reimplantation. Documentation chapitre 1-22 Figure 1-19
 typedef struct
 {
 	uint32_t r_offset;
@@ -24,18 +25,24 @@ typedef struct
 	int32_t r_addend; // Elf32_Sword
 } Elf32_Rela;
 
+// Structures pour stocker les informations d'une reimplantation : type, nom
 typedef struct
 {
+	uint32_t value;
 	char type[30];
+	char name[50];
 	Elf32_Rel rel;
 } Rel;
 
 typedef struct
 {
+	uint32_t value;
 	char type[30];
+	char name[30];
 	Elf32_Rela rela;
 } Rela;
 
+// Structure pour stocker une liste de reimplantation
 typedef struct
 {
 	char name[30];
@@ -46,6 +53,7 @@ typedef struct
 	Rela *relaTab;
 } RelocList;
 
+// Structure pour stocker une liste (table) de liste de reimplantation reimplantation
 typedef struct
 {
 	int nb_list;
@@ -53,20 +61,70 @@ typedef struct
 } RelocTable;
 
 
-Rela lire_rela(Section sect, int i);
+/*******************************************************************************
+ * lire_rela_liste
+ * parametres : Section sect, SectionsList liste_sect, SymbolesList liste_symb
+ * resultat : RelocList
+ * description : Lit dans la section sect une liste de reimplantation et la
+ *               renvoie sous la forme d'une structure dediee.
+ * effet de bord : aucun
+ *******************************************************************************/
+RelocList lire_rela_liste(Section sect, SectionsList liste_sect, SymbolesList liste_symb);
 
-Rel lire_rel(Section sect, int i);
 
-RelocList lire_rela_liste(Section sect);
+/*******************************************************************************
+ * lire_rel_liste
+ * parametres : Section sect, SectionsList liste_sect, SymbolesList liste_symb
+ * resultat : RelocList
+ * description : Lit dans la section sect une liste de reimplantation et la
+ *               renvoie sous la forme d'une structure dediee.
+ * effet de bord : aucun
+ *******************************************************************************/
+RelocList lire_rel_liste(Section sect, SectionsList liste_sect, SymbolesList liste_symb);
 
-RelocList lire_rel_liste(Section sect);
 
-RelocTable lire_reimp_table(SectionsList liste);
+/*******************************************************************************
+ * lire_reimp_table
+ * parametres : SectionsList liste_sect, SymbolesList liste_symb
+ * resultat : RelocTable
+ * description : Lit depuis le contenu des sections une table de reimplantation
+ *               et la renvoie sous la forme d'une structure dediee.
+ * effet de bord : aucun
+ *******************************************************************************/
+RelocTable lire_reimp_table(SectionsList liste_sect, SymbolesList liste_symb);
 
+
+/*******************************************************************************
+ * afficher_reimp_table
+ * parametres : RelocTable table_reimp
+ * resultat : aucun
+ * description : Affiche de facon lisible les informations de la table de
+ *               reimplantation donnee.
+ * effet de bord : aucun
+ *******************************************************************************/
 void afficher_reimp_table(RelocTable table_reimp);
 
-// void ecrire_reimp_table(SectionList liste_sect, RelocTable table_reimp);
 
+/*******************************************************************************
+ * ecrire_reimp_table
+ * parametres : SectionsList liste_sect, RelocTable table_reimp
+ * resultat : aucun
+ * description : Ecrit dans le contenu des sections de liste_sec le contenu de
+ *               la table des symboles definie dans la structure liste_symb.
+ * effet de bord : Modification de contenu de sections de liste_sect
+ *******************************************************************************/
+void ecrire_reimp_table(SectionsList liste_sect, RelocTable table_reimp);
+
+
+/*******************************************************************************
+ * supprimer_reimp_table
+ * parametres : RelocTable table_reimp
+ * resultat : aucun
+ * description : Libere l'espace memoire de la structure donnee.
+ * effet de bord : Liberation de memoire
+ *******************************************************************************/
 void supprimer_reimp_table(RelocTable table_reimp);
 
+
 #endif
+
