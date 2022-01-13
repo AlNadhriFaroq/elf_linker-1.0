5b90b07a0607a6336e863855ba6717e532c6d89c
Sakraft <95034115+Sakraft@users.noreply.github.com>
2022-01-13 11:19:28 +0100

    MAJ étape 7
    Mise à jour du module 'reimpRenumSect.c' et .h pour prendre en compte la correction des numéros de sections dans la table des symboles (partie 2 de l'étape 7).
    Mise à jour des commentaires et spécifications.
    Mise à jour du Makefile en conséquence.

--- Phase2/reimpRenumSect.h
+++ Phase2/reimpRenumSect.h
@@ -3,26 +3,28 @@
 #include <stdint.h>
 #include "../Phase1/readelfHeader.h"
 #include "../Phase1/readelfSectTable.h"
+#include "../Phase1/readelfSymbTable.h"
 
 
 /*******************************************************************************
  * supprimer_section
- * parametres : SectionsList * liste, int i
+ * parametres : SectionsList * liste, SymbolesList *liste_symb, int i
  * resultat : uint32_t
- * description : Modifie les sections et la table des sections en supprimant la
- *               la section i.
+ * description : Modifie les sections, la table des sections et la tables des
+ *               symboles en supprimant la section i.
  * effet de bord : Modifie le contenu de la structure donnee
  *******************************************************************************/
-uint32_t supprimer_section(SectionsList *liste, int i);
+uint32_t supprimer_section(SectionsList *liste_sect, SymbolesList *liste_symb, int i);
 
 
 /*******************************************************************************
  * renumeroter_sections
- * parametres : SectionsList * liste, Elf32_Ehdr *header
+ * parametres : SectionsList * liste, SymbolesList *liste_symb, Elf32_Ehdr *header
  * resultat : void
- * description : Modifie les sections, la table des sections et le header en
- *               suppriment les sections de type SH_REL et SH_RELA.
+ * description : Modifie les sections, la table des sections, la table des
+ *               symboles et le header en suppriment les sections de type
+ *               SH_REL et SH_RELA.
  * effet de bord : Modifie le contenu des structures donnees
  *******************************************************************************/
-void renumeroter_sections(SectionsList * liste, Elf32_Ehdr *header);
+void renumeroter_sections(SectionsList * liste_sect, SymbolesList *liste_symb, Elf32_Ehdr *header);
 
